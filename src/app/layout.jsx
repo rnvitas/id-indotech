8633329acb0030a7860c001e7149dda029c5c863
Rusydina Novitasari <rnvitas697@gmail.com>
2025-03-20 02:28:02 +0700

    fixed some error

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -8,13 +8,14 @@ import "../styles/update.css";
 // import "animate.css";
 import Navbar from "@/components/fragments/Navbar";
 import Footer from "@/components/fragments/Footer";
+const basePath = process.env.NEXT_PUBLIC_BASE_PATH || "";
 
 export const metadata = {
   title: "Indotech Digital Group",
   description:
     " A technology company specializing in software development,digital marketing, social media management, video production,and innovation in the digital and fintech industries. As atechnology consortium, Indotech Digital Group provides integrated digital solutions for various industries, ranging from education and entertainment to finance.",
   icons: {
-    icon: "/icon/favicon.png",
+    icon: `${basePath}/icon/favicon.png`,
   },
 };
 
--- src/components/landingPage/Hero.jsx
+++ src/components/landingPage/Hero.jsx
@@ -178,7 +178,7 @@ export default function Hero() {
 
       {showModal && selectedProduct && (
         <Modal
-          data={selectedProduct}
+          product={selectedProduct}
           closeModal={closeModal}
           showModal={showModal}
         />
